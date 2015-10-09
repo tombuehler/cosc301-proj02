bfafb5eaa70f5d576b6e5a7512da87eab3dcb654
cosc301 <jsimoni@colgate.edu>
2015-10-08 21:45:42 -0400

    implemented sequetial processing

--- main.c
+++ main.c
@@ -47,25 +47,58 @@ void removeComments(char* input){
     }
 }
 
-int arrLen(char** arr){
-    int i;
-    for(i = 0; arr[i] != NULL; i++){ } 
-    return i;
+//Parses commands sequentially
+int seqParse(char*** commands){
+    const char* whitespace = " \t\n";
+    char** arguments;
+    int state = 0;
+    int* status = 0;
+    pid_t pid;
+    for (int i = 0; (*commands)[i] != NULL; i++){
+        arguments = tokenify((*commands)[i], whitespace);
+        if (strcmp(arguments[0], "mode") == 0){         //check for built-in commands
+            if (strcmp(arguments[1], "p") == 0) state = 1;
+        }
+        else if (strcmp(arguments[0], "parallel") == 0)  state = 1;
+        else if (strcmp(arguments[0], "exit") == 0) state = 2;
+        else{
+            pid = fork();
+            if (pid == 0){   //child process
+                execv(arguments[0], arguments);     //execv the command
+                printf("Command not valid\n");      //if execv returns, command was invalid
+            exit(0);
+            }
+            else{   //parent process
+                waitpid(pid, status, 0);    // wait for child process to end
+            }
+            free(arguments);
+        }
+    }
+    return state;
 }
 
 int main(int argc, char **argv) {
-    const char* whitespace= " \t\n";
     char buffer[1024];
     int continueloop = 0;
     char** commands;
+    int state = 0; // 0 = sequential, 1 = parallel, 2 = exit
 
     while(continueloop == 0){
-        printf("Type away: ");
+        if (state == 0) printf("\nOperating in sequential mode\n");
+        else if (state == 1) printf("\nOperating in parallel mode\n");
+        printf("Type away>> ");
         fgets(buffer, 1024, stdin);
         removeComments(buffer);
         commands = tokenify(buffer, ";");
-        for (int i = 0; i < arrLen(commands); i++){
-            printf("Command %d: %s\n", i, commands[i]);
+        if (state == 0){
+            state = seqParse(&commands);
+        }
+        else if (state == 1){
+            //state = parParse(&commands);
+        }
+        if (state == 2){
+            printf("Goodbye");
+            return 0;
         }
     }
     return 0;
